d26e186135edb5e1427b09423bea211df313f7f4
Walfalcon <hyperlucas@gmail.com>
2017-06-14 08:55:37 -0700

    bx

--- dsgf/Game.java
+++ dsgf/Game.java
@@ -1,114 +1,129 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import javafx.embed.swing.JFXPanel;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.util.ArrayList;
 import javax.swing.JFrame;
+import javafx.scene.media.Media;
+import java.io.File;
+import javafx.util.Duration;
+import javafx.scene.media.MediaPlayer;
 
-public class Game extends JFrame implements Runnable{	
-	private static final long serialVersionUID = 1L;
-	public int mapWidth = 15;
-	public int mapHeight = 15;
-	public static int screenWidth = 640;
-	public static int screenHeight = 480;
-	private Thread thread;
-	private boolean running;
-	private BufferedImage image;
-	public int[] pixels;
-	public Camera camera;
-	public Screen screen;
-	public ArrayList<Texture> textures;
-	public GameObject rawr;
-	public static int[][] map = 
-		{
-			{1,1,1,1,1,1,1,1,2,2,2,2,2,2,2},
-			{1,0,0,0,0,0,0,0,2,0,0,0,0,0,2},
-			{1,0,3,3,3,3,3,0,0,0,0,0,0,0,2},
-			{1,0,3,0,0,0,3,0,2,0,0,0,0,0,2},
-			{1,0,3,0,0,0,3,0,2,2,2,0,2,2,2},
-			{1,0,3,0,0,0,3,0,2,0,0,0,0,0,2},
-			{1,0,3,3,0,3,3,0,2,0,0,0,0,0,2},
-			{1,0,0,0,0,0,0,0,2,0,0,0,0,0,2},
-			{1,1,1,1,1,1,1,1,4,4,4,0,4,4,4},
-			{1,0,0,0,0,0,1,4,0,0,0,0,0,0,4},
-			{1,0,0,0,0,0,1,4,0,0,0,0,0,0,4},
-			{1,0,0,2,0,0,1,4,0,3,3,3,3,0,4},
-			{1,0,0,0,0,0,1,4,0,3,3,3,3,0,4},
-			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
-			{1,1,1,1,1,1,1,4,4,4,4,4,4,4,4}
-		};
+public class Game extends JFrame implements Runnable{   
+    private static final long serialVersionUID = 1L;
+    public int mapWidth = 15;
+    public int mapHeight = 15;
+    public static int screenWidth = 640;//640
+    public static int screenHeight = 480;//480
+    private Thread thread;
+    private boolean running;
+    private BufferedImage image;
+    public int[] pixels;
+    private MediaPlayer audio;
+    public Camera camera;
+    public Screen screen;
+    public ArrayList<Texture> textures;
+    public GameObject rawr;
+    public static int[][] map = 
+        {
+            {1,1,1,1,1,1,1,1,2,2,2,2,2,2,2},
+            {1,0,0,0,0,0,0,0,2,0,0,0,0,0,2},
+            {1,0,3,3,3,3,3,0,0,0,0,0,0,0,2},
+            {1,0,3,0,0,0,3,0,2,0,0,0,0,0,2},
+            {1,0,3,0,0,0,3,0,2,2,2,0,2,2,2},
+            {1,0,3,0,0,0,3,0,2,0,0,0,0,0,2},
+            {1,0,3,3,0,3,3,0,2,0,0,0,0,0,2},
+            {1,0,0,0,0,0,0,0,2,0,0,0,0,0,2},
+            {1,1,1,1,1,1,1,1,4,4,4,0,4,4,4},
+            {1,0,0,0,0,0,1,4,0,0,0,0,0,0,4},
+            {1,0,0,0,0,0,1,4,0,0,0,0,0,0,4},
+            {1,0,0,2,0,0,1,4,0,3,3,3,3,0,4},
+            {1,0,0,0,0,0,1,4,0,3,3,3,3,0,4},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
+            {1,1,1,1,1,1,1,4,4,4,4,4,4,4,4}
+        };
 
 public Game() {
-		thread = new Thread(this);
-		rawr = new GameObject(5.5, 5.5, "res/rawr.png");
-		image = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_RGB);
-		pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
-		setSize(640, 480);
-		setResizable(false);
-		setTitle("3D Engine");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBackground(Color.black);
-		setLocationRelativeTo(null);
-		setVisible(true);
-		camera = new Camera(4.5, 4.5, 1, 0, 0, -.66);
-		addKeyListener(camera);
-		textures = new ArrayList<Texture>();
-		textures.add(Texture.wood);
-		textures.add(Texture.brick);
-		textures.add(Texture.bluestone);
-		textures.add(Texture.stone);
-		screen = new Screen(map, textures, 640, 480);
-		start();
-	}
+        thread = new Thread(this);
+        rawr = new GameObject(5, 6.01, "res/rawr.png");
+        image = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_RGB);
+        pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+        setSize(screenWidth, screenHeight);
+        setResizable(true);
+        setTitle("3D Engine");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(Color.black);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        camera = new Camera(4.5, 4.5, 1, 0, 0, -.66);
+        addKeyListener(camera);
+        textures = new ArrayList<Texture>();
+        textures.add(Texture.wood);
+        textures.add(Texture.brick);
+        textures.add(Texture.bluestone);
+        textures.add(Texture.stone);
+        screen = new Screen(map, textures, screenWidth, screenHeight);
+        final JFXPanel fxPanel = new JFXPanel();
+        this.add(fxPanel);
+        audio = new MediaPlayer(new Media(new File("res/audio.mp3").toURI().toString()));
+        audio.setOnEndOfMedia(new Runnable() {
+            public void run() {
+                audio.seek(Duration.ZERO);
+              }
+            });
+        start();
+    }
 
 private synchronized void start() {
-	running = true;
-	thread.start();
+    running = true;
+    audio.play();
+    thread.start();
 }
 public synchronized void stop() {
-	running = false;
-	try {
-		thread.join();
-	} catch(InterruptedException e) {
-		e.printStackTrace();
-	}
+    running = false;
+    try {
+        thread.join();
+    } catch(InterruptedException e) {
+        e.printStackTrace();
+    }
 }
 
 public void render() {
-	BufferStrategy bs = getBufferStrategy();
-	if(bs == null) {
-		createBufferStrategy(3);
-		return;
-	}
-	Graphics g = bs.getDrawGraphics();
-	g.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
-	rawr.render(g, camera);
-	bs.show();
+    BufferStrategy bs = getBufferStrategy();
+    if(bs == null) {
+        createBufferStrategy(3);
+        return;
+    }
+    Graphics g = bs.getDrawGraphics();
+    g.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
+    rawr.render(g, camera);
+    bs.show();
 }
 
 public void run() {
-	long lastTime = System.nanoTime();
-	final double ns = 1000000000.0 / 60.0;//60 times per second
-	double delta = 0;
-	requestFocus();
-	while(running) {
-		long now = System.nanoTime();
-		delta = delta + ((now-lastTime) / ns);
-		lastTime = now;
-		while (delta >= 1)//Make sure update is only happening 60 times a second
-		{
-			//handles all of the logic restricted time
-			camera.update(map);
-			screen.update(camera, pixels);
-			delta--;
-		}
-		render();//displays to the screen unrestricted time
-	}
+    long lastTime = System.nanoTime();
+    final double ns = 1000000000.0 / 60.0;//60 times per second
+    double delta = 0;
+    requestFocus();
+    while(running) {
+        long now = System.nanoTime();
+        delta = delta + ((now-lastTime) / ns);
+        lastTime = now;
+        while (delta >= 1)//Make sure update is only happening 60 times a second
+        {
+            //handles all of the logic restricted time
+            camera.update(map);
+            screen.update(camera, pixels);
+            delta--;
+        }
+        render();//displays to the screen unrestricted time
+    }
 }
 
 public static void main(String [] args) {
-	Game game = new Game();
+    Game game = new Game();
 }
 
 }
